5f776ba89e8028035cd5498357957c7ef79b2605
Riley Johnston <rileydanejohnston@gmail.com>
2021-10-28 18:11:28 -0500

    project setup - add json data and route files

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(404).send('Error');
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
 })
