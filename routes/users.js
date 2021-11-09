ff07eeb31e85675f1975a70b38ff3a9cfb9d81b7
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-09 15:25:56 -0600

    add getUser by id controller

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { getUsers, createUser } = require('../controllers/users');
+const { getUsers, getUser, createUser } = require('../controllers/users');
 
 router.get('/users', getUsers);
-//router.get('/users/:userId', getUser);
+router.get('/users/:userId', getUser);
 router.post('/users', createUser);
 
 /*
