9bec3c464bd84ecb1aa13de22c3596bb17f8ee24
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-08 23:02:14 -0600

    connect mongoose to app

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 app.use('/', userRouter);
 app.use('/', cardRouter);
