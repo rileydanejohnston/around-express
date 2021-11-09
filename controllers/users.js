68e9af78b65516de257dc7d5b0bc39f7ce331fa5
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-09 14:59:52 -0600

    add controller for user post request, parse jsons in app.js, add keyword 'new' to users model

---
+++ controllers/users.js
@@ -0,0 +1,15 @@
+// pull in the users model
+const Users = require('../models/user');
+
+// controller function - create new user in database from post request
+module.exports.createUser = (req, res) => {
+  // get user info from request body
+  const { name, about, avatar } = req.body;
+
+  // create a new document in db -> pass name, about, avatar
+  Users.create({ name, about, avatar })
+  // if successful, we get send the user back with 200 status
+  .then(user => res.status(200).send({ data: user }))
+  // if failure, we send error message back with 500 status
+  .catch(err => res.status(500).send({ message: 'There was a server error.'}));
+}
