278f49a37553a8fc59a5e519c6a21621ae8685b3
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-10 21:20:06 -0600

    add dislikeCard controller

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,15 @@
 const Cards = require('../models/card');
 
+module.exports.dislikeCard = (req, res) =>
+  Cards.findByIdAndUpdate(req.params.cardId,
+    { $pull: { likes: req.user._id } }, // remove _id from the array
+    { new: true },
+  )
+  .then(likes => res.status(200).send({ data: likes }))
+  .catch(err => {
+    res.status(500).send({ message: err });
+  });
+
 module.exports.likeCard = (req, res) => {
   Cards.findByIdAndUpdate(req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // add _id to the array if it's not there yet
@@ -7,7 +17,6 @@ module.exports.likeCard = (req, res) => {
   )
   .then(likes => res.status(200).send({ data: likes }))
   .catch(err => {
-    console.log(err.name);
     res.status(500).send({ message: err });
   });
 }
--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { getCards, createCard, deleteCard, likeCard } = require('../controllers/cards');
+const { getCards, createCard, deleteCard, likeCard, dislikeCard } = require('../controllers/cards');
 
 router.get('/cards', getCards);
 router.post('/cards', createCard);
 router.delete('/cards/:id', deleteCard);
 router.put('/cards/:cardId/likes', likeCard);
+router.delete('/cards/:cardId/likes', dislikeCard);
 
 /*
 const fs = require('fs');
